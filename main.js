2111ec551dfbecd7f124887f9b5c064cb2986599
Lauren Hoock <laurenh8916@gmail.com>
2020-02-11 06:06:13 -0700

    added random color start option and changed to have mouse passes darken the sketchpad by 10% of original value

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-// area is always going to be 500px
-// determine how large areas should be
 let sketchpadContainer = document.getElementById('sketchpadContainer');
 let resetBtn = document.getElementById('resetBtn');
 
 let divCount = 16;
 
+let startBackground = randomRGB();
+
 function createGrid() {
-    //let divSize = 500 / divCount;
     let divSize = 100 / divCount
     let divTotal = divCount * divCount;
     console.log(divTotal);
     
+    // Clear sketchpad
     while(sketchpadContainer.firstChild) {
         sketchpadContainer.removeChild(sketchpadContainer.firstChild);
     }
         
+    // Generate new sketchpad
     for (i = 0; i < divTotal; i++) {
         newDiv = document.createElement('div');
-        //newDiv.style.width = divSize + "px";
-        //newDiv.style.height = divSize + "px";
         newDiv.style.width = divSize + "%";
         newDiv.style.flexGrow = 1;
-        newDiv.style.backgroundColor = "grey";
+        newDiv.style.backgroundColor = "rgb(" + startBackground + ")";
         sketchpadContainer.appendChild(newDiv);
     }
+}
+
+function randomRGB() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return (r + ", " + g + ", " + b);
+}
+
+function shadesRGB() {
+    // Shades cells 10% darker each pass
+    sketchpadContainer.addEventListener("mouseover", function(e){
+        let colors = e.target.style.backgroundColor;
+        let rgbSlice = colors.slice(4, -1);
+        let rgbArray = rgbSlice.split(', ');
+
+        for (let i = 0; i < 3; i++) {
+            if (rgbArray[i] < 26) {
+                rgbArray[i] = 26;
+            }
+        }
+    
+        let rInc = rgbArray[0];
+        let gInc = rgbArray[1];
+        let bInc = rgbArray[2];
+    
+        rInc -= 26;
+        gInc -= 26;
+        bInc -= 26;
+
+        console.log(rInc, gInc, bInc);
+    
+        e.target.style.backgroundColor = "rgb(" + rInc + ", " + gInc + ", " + bInc + ")";
+    
+    });
+
 }
 
 createGrid();
@@ -33,6 +68,10 @@ resetBtn.addEventListener("click", function(){
     createGrid();
 });
 
-sketchpadContainer.addEventListener("mouseover", function(e){
-    e.target.style.backgroundColor = "blue";
-});
+colorBtn.addEventListener("click", function(){
+    divCount = prompt("How many squares wide should the grid be?");
+    startBackground = randomRGB();
+    createGrid();
+})
+
+shadesRGB();
